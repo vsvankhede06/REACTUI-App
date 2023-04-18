32b8f24bc92ce5536d0f4591b941238fd888fd7a
VandanaSonetel <Vandana.Vankhede@sonetel.com>
2023-04-18 10:14:08 +0530

    adding css properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
-import { Route, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./Componenets/Header";
+import Home from "./Componenets/Home";
+import "./styles/App.scss"
+import "./styles/header.scss"
+import "./styles/home.scss"
 
 function App() {
   return (
    <Router>
+    <Header/>
     <Routes>
-      <Route>
-
+      <Route path = "/" element = {<Home/>}>
       </Route>
     </Routes>
    </Router>
---
+++ src/Componenets/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='home' >
+            <main>
+                <h1>
+                    Cryster
+                </h1>
+                <p>
+                    Solution to all your problems
+                </p>
+            </main>
+        </div>
+    )
+}
+
+export default Home
